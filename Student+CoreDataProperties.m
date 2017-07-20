427548260fe898b8ed19b1c5d6709fb950325573
Burenkovuser <nb2207@yandex.ru>
2017-07-20 12:03:23 +0700

    part #1 end

--- Student+CoreDataProperties.m
+++ Student+CoreDataProperties.m
@@ -19,4 +19,38 @@
 @dynamic lastName;
 @dynamic score;
 
+/*
+//переопределяем сеттеры и геттеры
+- (void) setFirstName:(NSString *)firstName {
+    
+    [self willChangeValueForKey:@"firstName"];
+    [self setPrimitiveValue:firstName forKey:@"firstName"];
+    [self didChangeValueForKey:@"firsrName"];
+    
+    //NSLog(@"SET FIRST NAME!!!");
+    
+}
+
+- (NSString *) firstName {
+    
+    NSString *string = nil;
+    [self willAccessValueForKey:@"firstName"];
+    string = [self primitiveValueForKey:@"firstName"];
+    [self didAccessValueForKey:@"firstName"];
+    
+    //NSLog(@"GET FIRST NAME!!!");
+    
+    return string;
+}
+
+
+
+- (BOOL) validateLastName: (id  _Nullable __autoreleasing *)value error:(NSError * _Nullable __autoreleasing *) error{
+    
+    *error = [NSError errorWithDomain:@"BAD LAST NAME" code:123 userInfo:nil];
+    return  NO;
+}
+
+*/
+
 @end
